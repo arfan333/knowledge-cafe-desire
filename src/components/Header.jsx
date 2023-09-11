99b2e393ae8256725862d65ed01b860d28a5f668
arfan333 <arfantamim333@gmail.com>
2023-09-11 23:56:28 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-yellow-950 text-4xl font-bold'>React practice workshop : Knowledge cafe desire...</h1>
+        </div>
+    );
+};
+
+export default Header;
