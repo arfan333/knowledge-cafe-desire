d806d8acb9b9d51521007bb345f902577b2374d2
arfan333 <arfantamim333@gmail.com>
2023-09-12 13:16:11 +0600

    commit

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,31 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title,cover_img, reading_time, author, author_img, post_date,hashtags} = blog
     return (
-        <div>
-            
+        <div className='mb-16'>
+            <img className='w-full' src={cover_img} alt={`cover_img of the title ${title}`} />
+            <div className='flex justify-between justify-center items-center mt-8'>
+                <div className='flex justify-center gap-6'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div>
+                    <h3 className='text-xl font-bold'>{author}</h3>
+                    <p>{post_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button className='ml-2'><FaBookmark></FaBookmark></button>
+                    
+                </div>
+            </div>
+            <h2 className='text-2xl'>{title}</h2>
+            <p> 
+            {
+              hashtags.map((hash, idx) => <span key={idx}><a href=''>#{hash}</a> </span>)  
+            }
+            </p>
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import profile from '../assets/images/profile.png'
 const Header = () => {
     return (
-        <header className='flex justify-around items-center p-4 mx-4 border-b-4'>
+        <header className='flex justify-around items-center p-4 max-w-6xl mx-auto border-b-4'>
              <h1 className='text-yellow-950 text-4xl font-bold'>React practice workshop : Knowledge cafe desire...</h1>
              <img src={profile} alt="" />
         </header>
